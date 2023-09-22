6bcee164049671e5b7b66275bfbb9cc674bb9658
Dmitrii <dmitrii.nazarov7703@gmail.com>
2023-09-22 10:54:30 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Dmitrii Nazarov on 18.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
